7a06d35f65b9fc2dad6908fceaab6139b89b2217
Michael Petitjean <petitjm@ambrio.com>
2026-04-13 17:14:37 +0100

    Fix Supabase confirm route

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -4,13 +4,9 @@ import { createClient } from '@/lib/supabase/server'
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
+
   const token_hash = searchParams.get('token_hash')
   const type = searchParams.get('type') as EmailOtpType | null
-
-  const redirectTo = request.nextUrl.clone()
-  redirectTo.pathname = '/'
-  redirectTo.searchParams.delete('token_hash')
-  redirectTo.searchParams.delete('type')
 
   if (token_hash && type) {
     const supabase = await createClient()
@@ -21,7 +17,7 @@ export async function GET(request: NextRequest) {
     })
 
     if (!error) {
-      return NextResponse.redirect(redirectTo)
+      return NextResponse.redirect(new URL('/', request.url))
     }
   }
 
