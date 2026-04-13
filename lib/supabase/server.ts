5a2d78f617bb437a7b292f6c6775c0e4682964a8
Michael Petitjean <petitjm@ambrio.com>
2026-04-13 17:22:18 +0100

    Fix Supabase server cookies

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -13,11 +13,9 @@ export async function createClient() {
           return cookieStore.getAll()
         },
         setAll(cookiesToSet) {
-          try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
-            )
-          } catch {}
+          cookiesToSet.forEach(({ name, value, options }) =>
+            cookieStore.set(name, value, options)
+          )
         },
       },
     }
