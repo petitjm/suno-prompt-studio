dc4951892c5b551a084f23d622e1cbfcc6ffabc0
Michael Petitjean <petitjm@ambrio.com>
2026-04-22 19:15:03 +0100

    New AI rehearse old JSX in LegacyRehearseUI tke out return

--- components/LegacyRehearseUI.tsx
+++ components/LegacyRehearseUI.tsx
@@ -1,10 +1,6 @@
 'use client'
 
-import React from 'react'
-
 export default function LegacyRehearseUI() {
-  return <div>Legacy rehearse UI placeholder</div>
-}
   return (
     <div style={pageStyle}>
       <div style={sidebarStyle}>
