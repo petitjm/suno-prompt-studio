da01a8ba06b5929b614bda39319971b4b18e9a96
Michael Petitjean <petitjm@ambrio.com>
2026-05-25 14:22:54 +0100

    Clarify clear chord editor button label

--- components/StructuredChordJsonEditor.tsx
+++ components/StructuredChordJsonEditor.tsx
@@ -130,7 +130,7 @@ const chordJsonIsValidObject = React.useMemo(() => {
   disabled={savingChords || !chordsText.trim()}
   className="px-4 py-2 rounded bg-gray-700 hover:bg-gray-600 disabled:opacity-50 disabled:cursor-not-allowed text-white whitespace-nowrap"
 >
-  Clear JSON
+  Clear chord editor
 </button>
 
 </div>
