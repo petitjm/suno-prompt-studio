3ca142457201d2f9773dadc4434ec83636768117
Michael Petitjean <petitjm@ambrio.com>
2026-05-31 18:47:54 +0100

    Reset selected chord version before saving

--- components/StructuredChordJsonEditor.tsx
+++ components/StructuredChordJsonEditor.tsx
@@ -126,7 +126,10 @@ const chordJsonIsValidObject = React.useMemo(() => {
 
   <button
     type="button"
-    onClick={saveChords}
+    onClick={() => {
+      setActiveChordVersionId(null)
+      saveChords()
+    }}
     disabled={savingChords || !chordJsonIsValidObject}
     className="px-4 py-2 rounded bg-blue-600 hover:bg-blue-500 disabled:opacity-50 disabled:cursor-not-allowed text-white whitespace-nowrap"
   >
