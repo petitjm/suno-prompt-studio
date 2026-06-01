bf017ea72c16048c4418d842deb77a6c197eacea
Michael Petitjean <petitjm@ambrio.com>
2026-06-01 11:56:01 +0100

    Clarify disabled chord save button

--- components/StructuredChordJsonEditor.tsx
+++ components/StructuredChordJsonEditor.tsx
@@ -129,12 +129,19 @@ const chordJsonIsValidObject = React.useMemo(() => {
     type="button"
     onClick={() => {
       setActiveChordVersionId(null)
+      setChordExtractionMessage('')
       saveChords()
     }}
     disabled={savingChords || !chordJsonIsValidObject}
     className="px-4 py-2 rounded bg-blue-600 hover:bg-blue-500 disabled:opacity-50 disabled:cursor-not-allowed text-white whitespace-nowrap"
   >
-    {savingChords ? 'Saving...' : justSavedChords ? 'Saved' : 'Save Chords'}
+    {savingChords
+      ? 'Saving...'
+      : justSavedChords
+        ? 'Saved ✓'
+        : !chordJsonIsValidObject
+          ? 'Enter valid chord JSON'
+          : 'Save Chords'}
   </button>
 
   <button
