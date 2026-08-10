3e1b55dfb9082ccab63a2d27cb3b659987cf89a5
Michael Petitjean <petitjm@ambrio.com>
2026-08-10 09:28:40 +0100

    Reset audio preview when chord editor clears

--- components/StructuredChordJsonEditor.tsx
+++ components/StructuredChordJsonEditor.tsx
@@ -179,6 +179,7 @@ export default function StructuredChordJsonEditor({
             }
 
             setChordsText("");
+            resetAudioPreviewRequestState();
             setChordVersionTitle("");
             setChordExtractionMessage("");
             setActiveChordVersionId(null);
